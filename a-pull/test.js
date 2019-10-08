5e69f84bffcf034c17d92e98aba8f67c67299d6c
laras126 <laras126@gmail.com>
2019-10-08 08:15:33 -0400

    feat: add a-pull

---
+++ a-pull/test.js
@@ -0,0 +1,16 @@
+
+describe( '.a-pull', () => {
+	const pullSize = getCustomPropertyValue( '.a-pull', '--a-pull-size' );
+	const pullItem = $( '.a-pull-up-item' ).getBoundingClientRect();
+	const pullItemDirectParent = $( '.a-pull-up-item' ).parentElement.getBoundingClientRect();
+	const itemAbovePullItem = $( '.a-pull-up-above-item' ).getBoundingClientRect();
+	
+	it( 'pulls a box up, above its direct parent', () => {
+		return assertGreaterThan( pullItem.top.toFixed(), pullItemDirectParent.top.toFixed() );
+	});
+	
+	it( 'the above-item increases in height by the pull-size value', () => {
+		return assertEquals( pullItem.top.toFixed(), itemAbovePullItem.bottom.toFixed() );
+	});
+
+});
